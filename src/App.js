078c933344c8ee1d61e10128ef6584af5fa79440
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-11-23 10:59:10 -0600

    Added projects.js and .css. began styling on page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from "./Components/Home/Home.js"
 import About from "./Components/About/About.js"
+import Projects from "./Components/Projects/Projects.js"
 class App extends Component {
   render() {
     return (
@@ -10,6 +11,7 @@ class App extends Component {
             <div className="App">
                 <Route exact path="/" component={Home} />
                 <Route path="/about" component={About} />
+                <Route path="/projects" component={Projects} />
             </div>
         </BrowserRouter>
     );
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import "./About.css";
+import "./Projects.css";
 import {Link} from 'react-router-dom'
-class About extends React.Component{
+class Projects extends React.Component{
     constructor(props) {
         super(props)
     }
 
     render(){
         return(
-            <div className="" id="aboutContainer">
+            <div className="" id="projectsContainer">
                 <div className="">
                     <div className="">
-                        <div className="" id="aboutText">
+                        <div className="" id="projectsScroll">
                             <h1>about.</h1>
                             <h2>My name is Shawn Baugh II and I am an experienced Web Application Developer with a demonstrated history of working with many technologies. Skilled in Python, JavaScript, Java, and C# languages. A recent graduate from Coding Dojo Dallas with an interest in web and mobile app development. I enjoy creating solutions to make a client’s life easier. I am interested in learning more about a position with a company as a web or software developer.</h2>
                         </div>
-                        <div className="" id="aboutNav">
-                            <div id="projectsAboutNav">
-                                <h2><Link to="/projects">projects.</Link></h2>
+                        <div className="" id="projectsNav">
+                            <div id="aboutProjectsNav">
+                                <h2><Link to="/about">about.</Link></h2>
                             </div>
                             <div id="homeAboutNav">
                                 <h2><Link to="/">home.</Link></h2>
@@ -29,4 +29,4 @@ class About extends React.Component{
         )
     }
 }
-export default About;
+export default Projects;
