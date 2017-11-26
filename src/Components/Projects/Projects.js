5a8385f7d74a369ea2de2ea6c90c0e624fbd209c
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-11-25 18:56:22 -0600

    Adding and organizing projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Projects.css";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { VideoPlayer } from 'react-video-players';
 class Projects extends React.Component{
     constructor(props) {
         super(props)
@@ -9,20 +10,24 @@ class Projects extends React.Component{
     render(){
         return(
             <div className="" id="projectsContainer">
-                <div className="">
-                    <div className="">
-                        <div className="" id="projectsScroll">
-                            <h1>about.</h1>
-                            <h2>My name is Shawn Baugh II and I am an experienced Web Application Developer with a demonstrated history of working with many technologies. Skilled in Python, JavaScript, Java, and C# languages. A recent graduate from Coding Dojo Dallas with an interest in web and mobile app development. I enjoy creating solutions to make a client’s life easier. I am interested in learning more about a position with a company as a web or software developer.</h2>
+                <div className="" id="projectsScroll">
+                    <h1>projects.</h1>
+                        <div>
+                            <h2>Text about my projects...</h2>
                         </div>
-                        <div className="" id="projectsNav">
-                            <div id="aboutProjectsNav">
-                                <h2><Link to="/about">about.</Link></h2>
-                            </div>
-                            <div id="homeAboutNav">
-                                <h2><Link to="/">home.</Link></h2>
+                        <div id="project">
+                            <h2></h2>
+                            <div id="projectImage">
+                                <img src={require('./img/ideas_bg.png')} />
                             </div>
                         </div>
+                </div>
+                <div className="" id="aboutNav">
+                    <div id="projectsAboutNav">
+                        <h2><Link to="/about">about.</Link></h2>
+                    </div>
+                    <div id="homeAboutNav">
+                        <h2><Link to="/">home.</Link></h2>
                     </div>
                 </div>
             </div>
